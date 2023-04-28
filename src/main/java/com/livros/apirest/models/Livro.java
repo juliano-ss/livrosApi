537b1154f4dbc0200cf9c694d5d8f054fb1a8571
juliano-ss <jotaliano@live.com>
2023-04-28 15:33:05 -0300

    attributes book class

--- src/main/java/com/livros/apirest/models/Livro.java
+++ src/main/java/com/livros/apirest/models/Livro.java
@@ -1,13 +1,31 @@
 package com.livros.apirest.models;
 
 import java.io.Serializable;
+import java.math.BigDecimal;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 
 @Entity
 @Table(name="TB-LIVROS")
 public class Livro implements Serializable {
+	
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	//cria o id automanticamente 
+	
+	private long id;
+	
+	private String nome;
+	
+	private BigDecimal quantidade;
+	
+	private BigDecimal valor;
 	
 
 }
