255d78944694cfab474616938fa3af42ef28d0e1
juliano-ss <jotaliano@live.com>
2023-04-28 15:34:36 -0300

    getters and setters

--- src/main/java/com/livros/apirest/models/Livro.java
+++ src/main/java/com/livros/apirest/models/Livro.java
@@ -23,6 +23,38 @@ public class Livro implements Serializable {
 	
 	private String nome;
 	
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public BigDecimal getQuantidade() {
+		return quantidade;
+	}
+
+	public void setQuantidade(BigDecimal quantidade) {
+		this.quantidade = quantidade;
+	}
+
+	public BigDecimal getValor() {
+		return valor;
+	}
+
+	public void setValor(BigDecimal valor) {
+		this.valor = valor;
+	}
+
 	private BigDecimal quantidade;
 	
 	private BigDecimal valor;
