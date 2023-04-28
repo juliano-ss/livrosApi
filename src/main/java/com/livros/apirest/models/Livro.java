16ac9a50395c229bf47b3f07fdb511225937e35c
juliano-ss <jotaliano@live.com>
2023-04-28 15:27:01 -0300

    implements

--- src/main/java/com/livros/apirest/models/Livro.java
+++ src/main/java/com/livros/apirest/models/Livro.java
@@ -1,10 +1,13 @@
 package com.livros.apirest.models;
 
+import java.io.Serializable;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.Table;
 
 @Entity
 @Table(name="TB-LIVROS")
-public class Livro {
+public class Livro implements Serializable {
+	
 
 }
