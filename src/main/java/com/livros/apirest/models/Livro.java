64266a30f0f1fec083cd160a94e5726e914d5baf
juliano-ss <jotaliano@live.com>
2023-04-28 18:54:25 -0300

    update connection class

--- src/main/java/com/livros/apirest/models/Livro.java
+++ src/main/java/com/livros/apirest/models/Livro.java
@@ -10,18 +10,21 @@ import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 
 @Entity
-@Table(name="TB-LIVROS")
+@Table(name="TB_LIVROS")
 public class Livro implements Serializable {
 	
 	private static final long serialVersionUID = 1L;
 	
 	@Id
 	@GeneratedValue(strategy=GenerationType.AUTO)
-	//cria o id automanticamente 
-	
 	private long id;
 	
 	private String nome;
+	
+    private BigDecimal quantidade;
+	
+	private BigDecimal valor;
+	
 	
 	public long getId() {
 		return id;
@@ -55,9 +58,5 @@ public class Livro implements Serializable {
 		this.valor = valor;
 	}
 
-	private BigDecimal quantidade;
-	
-	private BigDecimal valor;
-	
 
 }
