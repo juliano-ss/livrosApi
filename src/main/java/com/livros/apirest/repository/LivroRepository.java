1b4d8d4841f1caaa9d19a49d353773078ebe846e
juliano-ss <jotaliano@live.com>
2023-05-01 22:32:45 -0300

    update Jpa

--- src/main/java/com/livros/apirest/repository/LivroRepository.java
+++ src/main/java/com/livros/apirest/repository/LivroRepository.java
@@ -1,11 +1,9 @@
 package com.livros.apirest.repository;
 
-import java.math.BigDecimal;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.livros.apirest.models.Livro;
 
-public interface LivroRepository extends JpaRepository<Livro, BigDecimal>{
+public interface LivroRepository extends JpaRepository<Livro, Long>{
 
 }
