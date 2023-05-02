64902be888d8570157908d11e31f6f490f670f9c
juliano-ss <jotaliano@live.com>
2023-05-01 22:44:22 -0300

    conf methods

--- src/main/java/com/livros/apirest/resource/LivroResource.java
+++ src/main/java/com/livros/apirest/resource/LivroResource.java
@@ -1,10 +1,16 @@
 package com.livros.apirest.resource;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.livros.apirest.repository.LivroRepository;
 
 @RestController
 @RequestMapping(value="/api")
 public class LivroResource {
+	
+	@Autowired
+	LivroRepository livroRepository;
 
 }
