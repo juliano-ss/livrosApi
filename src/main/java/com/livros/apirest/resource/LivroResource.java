2442e337fa0efb59f8c187eb6f8127e3a62d5ce8
juliano-ss <jotaliano@live.com>
2023-05-02 22:21:27 -0300

    imports

--- src/main/java/com/livros/apirest/resource/LivroResource.java
+++ src/main/java/com/livros/apirest/resource/LivroResource.java
@@ -1,9 +1,13 @@
 package com.livros.apirest.resource;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.livros.apirest.models.Livro;
 import com.livros.apirest.repository.LivroRepository;
 
 @RestController
@@ -14,7 +18,7 @@ public class LivroResource {
 	LivroRepository livroRepository;
 	
 	@GetMapping("/livros")
-	public List<Livros> listaLivros(){
+	public List<Livro> listaLivros(){
 		
 	}
 
