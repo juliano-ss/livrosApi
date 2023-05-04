718fe286a0fe0322700946d7cc39d93d55874348
juliano-ss <jotaliano@live.com>
2023-05-03 23:25:38 -0300

    find by id

--- src/main/java/com/livros/apirest/repository/LivroRepository.java
+++ src/main/java/com/livros/apirest/repository/LivroRepository.java
@@ -5,5 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.livros.apirest.models.Livro;
 
 public interface LivroRepository extends JpaRepository<Livro, Long>{
+	
+	Livro findById(long id);
 
 }
