0a6f54f888837f5318878c3e5da27bb9a3787c83
juliano-ss <jotaliano@live.com>
2023-05-03 23:21:21 -0300

    new method

--- src/main/java/com/livros/apirest/resource/LivroResource.java
+++ src/main/java/com/livros/apirest/resource/LivroResource.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -20,6 +21,11 @@ public class LivroResource {
 	@GetMapping("/livros")
 	public List<Livro> listaLivros(){
 		return livroRepository.findAll();
+		
+		
+		@GetMapping("/livro/{id}")
+		public Livro listaLivroUnico(@PathVariable(value="id") long id){
+			return livroRepository.findAll();	
 	}
 
 }
